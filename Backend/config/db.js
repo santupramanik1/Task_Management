d8f0c318f6a1d380094f0e8235c6f313a7380067
Santu pramanik <santu700141@gmail.com>
2025-06-22 18:57:47 +0530

    Configure MongoDB Atlas

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connDB = async () => {
     try {
         // await mongoose.connect("mongodb://127.0.0.1:27017/taskFlow")
-        await mongoose.connect("mongodb+srv://santu700141:TVFDFmoO3oZFHMUH@cluster0.vqysbs9.mongodb.net/taskPilot?retryWrites=true&w=majority")
-        console.log("Connected to mongodb")
+        await mongoose.connect("mongodb+srv://santu700141:santu7001@cluster0.iodot87.mongodb.net/TaskPilot")
+        // console.log("Connected to mongodb")
     } catch (error) {
         console.log(error)
     }
