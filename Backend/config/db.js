ca2810d06847a5dde188a11cab2bf68112e0c617
Santu pramanik <santu700141@gmail.com>
2025-06-22 19:00:41 +0530

    Setup Environment varibale

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
+
 export const connDB = async () => {
     try {
-        // await mongoose.connect("mongodb://127.0.0.1:27017/taskFlow")
-        await mongoose.connect("mongodb+srv://santu700141:santu7001@cluster0.iodot87.mongodb.net/TaskPilot")
-        // console.log("Connected to mongodb")
+        await mongoose.connect(process.env.MONGODB_URI)
     } catch (error) {
         console.log(error)
     }
